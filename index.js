940a944d6d47750c2e4f900bdc4314fb5bafe613
Chris Casey <cdcasey@gmail.com>
2018-02-22 16:42:16 -0600

    Excercise completed two ways.

--- index.js
+++ index.js
@@ -1,5 +1,56 @@
-(function() {
+/*(function () {
   'use strict';
 
-  // YOUR CODE HERE
+  var controls = document.querySelector('#controls');
+
+  var red = document.querySelector('#stopLight');
+  var yellow = document.querySelector('#slowLight');
+  var green = document.querySelector('#goLight');
+
+  function switchLights(color) {
+
+  }
+  controls.addEventListener('click', function (ev) {
+    console.log(ev.target);
+    if (ev.target.id === "stopButton") {
+      red.classList.add("stop");
+      yellow.classList.remove("slow");
+      green.classList.remove("go");
+    }
+    else if (ev.target.id === "slowButton") {
+      yellow.classList.add("slow");
+      red.classList.remove("stop");
+      green.classList.remove("go");
+    }
+    else if (ev.target.id === "goButton") {
+      green.classList.add("go");
+      yellow.classList.remove("slow");
+      red.classList.remove("stop");
+    }
+  })
+})();
+*/
+
+(function () {
+  'use strict';
+
+  let lights = {
+    stop: document.querySelector("#stopLight"),
+    slow: document.querySelector("#slowLight"),
+    go: document.querySelector("#goLight"),
+  }
+
+  let buttonDiv = document.querySelector("#controls");
+
+  buttonDiv.addEventListener('click', (event) => {
+    let lightBehavior = event.target.innerText.toLowerCase();
+    for (const key in lights) {
+      if (lights.hasOwnProperty(key)) {
+        const light = lights[key];
+        lights[key].setAttribute('class', 'bulb');
+      }
+      lights[lightBehavior].classList.add(lightBehavior);
+    }
+  })
+
 })();
